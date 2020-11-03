e132033eaefa243a6fa5ab13c94c6396c156ce45
Thiago dos Reis <henemesis@gmail.com>
2020-11-02 22:20:52 -0300

    feat[#06] Implementado Cap06 Exercício 01
    Implementação do capítulo 6, exercício 01 sobre o laço FOR do curso de Java Básico da AlgaWorks

--- AlgaJBasico/Cap6/com.exerc-divisiveis3/Cap6Exer1DivisiveisPor3.java
+++ AlgaJBasico/Cap6/com.exerc-divisiveis3/Cap6Exer1DivisiveisPor3.java
@@ -1,3 +1,26 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class Cap6Exer1DivisiveisPor3 {
 //    Exercício
+
+    public static void main(String[] args) {
+
+        Integer [] numeros = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
+        Integer divisivelPor3 = 0;
+        Integer [] retorno = new Integer[20];
+
+        System.out.print("Da lista acima, são divisíveis por três: {");
+        for (int i = 0; i < numeros.length ; i++) {
+            Integer numero = numeros[i];
+            divisivelPor3 = numero % 3;
+
+
+            if (divisivelPor3.equals(0)){
+                System.out.print(" " + numero);
+            }
+        }
+        System.out.print(" }");
+
+    }
 }
