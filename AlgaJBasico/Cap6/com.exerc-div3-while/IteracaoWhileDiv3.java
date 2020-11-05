cb5497bafa72f81d74f077f1a956cbbb51baf796
Thiago dos Reis <henemesis@gmail.com>
2020-11-04 23:06:23 -0300

    feat[#06] Implementado Cap06 Exercício 02
    Implementação do capítulo 6, exercício 01 sobre o laço WHILE do curso de Java Básico da AlgaWorks

---
+++ AlgaJBasico/Cap6/com.exerc-div3-while/IteracaoWhileDiv3.java
@@ -0,0 +1,28 @@
+import sun.rmi.server.InactiveGroupException;
+
+import java.util.Arrays;
+
+public class IteracaoWhileDiv3 {
+
+    public static void main(String [] args){
+        Integer[] numeros = new Integer[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
+
+        int i = 0;
+        int j = 0;
+        Integer divisivelPor3 = 0;
+        Integer numero = 0;
+        Integer [] armazena = new Integer[21];
+        Integer [] resultado = new Integer[21];
+
+        while (i < numeros.length) {
+            numero = numeros[i];
+            divisivelPor3 = numero % 3;
+
+            if (divisivelPor3.equals(0)) {
+                System.out.println(numero);
+            }
+            i++;
+        }
+
+    }
+}
