9957c524c4319c10067110d95ba28f204e869d85
henemesis <34524169+henemesis@users.noreply.github.com>
2021-01-13 15:21:25 -0300

    feat[#15]: Exercícios -- Arrays Unidimensionais
    exercício nº 2 sobre Arrays Unidimensionais.

---
+++ AlgaJBasico/exercicios-revisao/com.exercicios-revisao-array-uni/Exercicio_02.java
@@ -0,0 +1,15 @@
+package com.javaBasico;
+
+import java.util.Arrays;
+
+public class Exercicio_02 {
+
+    public static void main(String[] args) {
+        int a[] = new int[6];
+        for (int i = 0; i < a.length ; i++) {
+            a[i] += i;
+
+        }
+        System.out.print("O vetor de 6 posições terá os seguintes valores: " + Arrays.toString(a));
+    }
+}
