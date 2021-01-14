892d709d2b9f29ad00b156a770231c82d5432664
henemesis <34524169+henemesis@users.noreply.github.com>
2021-01-14 08:21:19 -0300

    feat[#16]: Exercícios -- Arrays Unidimensionais
    correção do exercício nº 2 sobre Arrays Unidimensionais.

--- AlgaJBasico/exercicios-revisao/com.exercicios-revisao-array-uni/Exercicio_02.java
+++ AlgaJBasico/exercicios-revisao/com.exercicios-revisao-array-uni/Exercicio_02.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class Exercicio_02 {
 
     public static void main(String[] args) {
-        int a[] = new int[6];
+        double [] a = new double[6];
         for (int i = 0; i < a.length ; i++) {
             a[i] += i;
 
